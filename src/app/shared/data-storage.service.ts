0b6005969d0253c8eadc75cd7b9139ccb072454f
Daniel <daniel.soots@frgrisk.com>
2022-06-23 09:02:38 -0400

    bringing repo current

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { map, tap } from "rxjs";
+import { Recipe } from "../recipes/recipe.model";
+import { RecipeService } from "../recipes/recipe.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataStorageService {
+  constructor(private http: HttpClient,
+              private recipeService: RecipeService) {
+
+  };
+
+
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put('https://ng-recipe-book-b639e-default-rtdb.firebaseio.com/recipes.json', recipes)
+      .subscribe(
+        (response) => {
+          console.log(response);
+      });
+  }
+
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>('https://ng-recipe-book-b639e-default-rtdb.firebaseio.com/recipes.json')
+      .pipe(
+        map(mapRecipes => {
+          return mapRecipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        }),
+        tap(recipes => {
+          this.recipeService.setReceipes(recipes)
+        })
+      )
+  }
+}
+
